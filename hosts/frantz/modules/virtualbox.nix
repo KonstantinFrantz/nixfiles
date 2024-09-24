999d7790e1738b6b4933a283aba25596a488accd
KonstantinFrantz <356156@edu.itmo.ru>
2024-09-24 09:29:11 +0300

    add virtual box extention pack

--- hosts/frantz/modules/virtualbox.nix
+++ hosts/frantz/modules/virtualbox.nix
@@ -1,4 +1,8 @@
 {
+  nixpkgs.config.allowUnfree = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
   virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.clipboard = true;
   users.extraGroups.vboxusers.members = ["frantz"];
 }
