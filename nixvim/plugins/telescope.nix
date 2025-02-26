b77a51ef798c84143dd832dc230588df745deec3
KonstantinFrantz <356156@edu.itmo.ru>
2025-02-26 18:22:31 +0300

    fix: enable web-devicons

--- nixvim/plugins/telescope.nix
+++ nixvim/plugins/telescope.nix
@@ -1,27 +1,30 @@
 {
-  programs.nixvim.plugins.telescope = {
-    enable = true;
-    keymaps = {
-      "<leader>ff" = "find_files";
-      "<C-p>" = "git_files";
-      "<leader>fg" = "live_grep";
-      "<leader>fr" = "lsp_references";
-    };
-    settings.defaults = {
-      set_env.COLORTERM = "truecolor";
-      file_ignore_patterns = [
-        "^.git/"
-        "^.mypy_cache/"
-        "^__pycache__/"
-        "^output/"
-        "^data/"
-        "%.ipynb"
-      ];
-      layout_config = {
-        horizontal = {
-          width = 0.95;
-          preview_cutoff = 0;
-          preview_width = 0.6;
+  programs.nixvim.plugins = {
+    web-devicons.enable = true;
+    telescope = {
+      enable = true;
+      keymaps = {
+        "<leader>ff" = "find_files";
+        "<C-p>" = "git_files";
+        "<leader>fg" = "live_grep";
+        "<leader>fr" = "lsp_references";
+      };
+      settings.defaults = {
+        set_env.COLORTERM = "truecolor";
+        file_ignore_patterns = [
+          "^.git/"
+          "^.mypy_cache/"
+          "^__pycache__/"
+          "^output/"
+          "^data/"
+          "%.ipynb"
+        ];
+        layout_config = {
+          horizontal = {
+            width = 0.95;
+            preview_cutoff = 0;
+            preview_width = 0.6;
+          };
         };
       };
     };
