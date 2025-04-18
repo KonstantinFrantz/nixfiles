ee69b7d08b574163d6e5766e4855fd7e16b5e90e
KonstantinFrantz <356156@edu.itmo.ru>
2025-04-18 16:33:39 +0300

    feat: delete virtualbox

--- hosts/frantz/modules/virtualbox.nix
+++
@@ -1,8 +0,0 @@
-{
-  nixpkgs.config.allowUnfree = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.guest.clipboard = true;
-  virtualisation.virtualbox.guest.enable = true;
-  users.extraGroups.vboxusers.members = ["frantz"];
-}
