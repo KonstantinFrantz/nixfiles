e16eabd85da6f89fcef1593de9c251b3947d3f5c
KonstantinFrantz <356156@edu.itmo.ru>
2025-06-02 17:35:45 +0300

    feat: system update

--- hosts/frantz/modules/hyprland.nix
+++ hosts/frantz/modules/hyprland.nix
@@ -3,10 +3,11 @@
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    # configPackages = with pkgs; [xdg-desktop-portal-gtk];
+    configPackages = with pkgs; [xdg-desktop-portal-gtk xdg-desktop-portal-hyprland];
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       xdg-desktop-portal-hyprland
     ];
+    config.common.default = "*";
   };
 }
